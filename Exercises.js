50c46f6bd2ec96ec58c6d34c1816e8bc37fadfe0
saad alhashimi <saad.alhashimi86@gmail.com>
2019-03-19 14:46:18 +0100

    first push

---
+++ Exercises.js
@@ -0,0 +1,16 @@
+//exercise data type
+let myText = 'hello_from_JavaScript'
+console.log(myText);
+console.log(myText[1], myText[5])
+let name = 'Saad';
+let age = 32;
+let year = 2019;
+let myInfo = name + ' is ' + age + ' in '+ year;
+console.log(myInfo);
+let isMarried = true;
+console.log(name + ' is married: ' + isMarried);
+let programmingLanguage = 'JavaScript';
+let isFun = true; 
+console.log(programmingLanguage + ' is fun: ' + isFun);
+console.log(programmingLanguage[0], programmingLanguage[4]);
+console.log(programmingLanguage.length);

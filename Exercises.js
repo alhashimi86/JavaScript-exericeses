f7a12063a6783dfcdfa4617e34b2732725d4bfd8
saad alhashimi <saad.alhashimi86@gmail.com>
2019-03-20 14:28:35 +0100

    new update

--- Exercises.js
+++ Exercises.js
@@ -1,5 +1,22 @@
+//exercise to getting started
+/*console.log('this is an exercise in console logging');
+let myVar = "this is exercise in console logging";
+console.log(myVar);
+let firstName = 'Saad',
+    lastName = 'Al Hashimi';
+
+ console.log(firstName, lastName);
+ 
+ let name = 'John',
+     familyName = 'Smith',
+     city = 'Berlin',
+     age = 43,
+     job = 'teacher';
+console.log(name + ' '+ familyName +' is a '+ age + ' year old '+ job + ' who lives in '+ city);
+console.log(`${name} ${familyName} is a ${age} year old ${job} who lives in ${city} `);*/
+
 //exercise data type
-let myText = 'hello_from_JavaScript'
+/*let myText = 'hello_from_JavaScript'
 console.log(myText);
 console.log(myText[1], myText[5])
 let name = 'Saad';
@@ -13,4 +30,31 @@ let programmingLanguage = 'JavaScript';
 let isFun = true; 
 console.log(programmingLanguage + ' is fun: ' + isFun);
 console.log(programmingLanguage[0], programmingLanguage[4]);
-console.log(programmingLanguage.length);
+console.log(programmingLanguage.length);*/
+
+//exercise data type 2
+//let length = 16.78; // number
+/*let lastName = "Johnson"; //string
+let car = ""; //strig
+let isOpen = true; //boolean
+let ageMark = 30;
+let ageJohn ="43"
+//console.log(typeof(ageMark));
+let olderThan = true;
+let myMessage = "John is older than Mark: "+ olderThan;
+//console.log(myMessage);
+
+let x ="this type of value";
+//console.log(x[x.length-1]);
+let a = 40;
+let b = 35;
+let c = 40;
+//console.log(a==b);
+//console.log(a==c);
+console.log(c==b);
+console.log(typeof(a==b));
+console.log(typeof(Infinity));
+console.log(isNaN(x));
+console.log(isNaN(a));
+console.log("variable is NOT a number: "+ isNaN(x))
+console.log(typeof(isNaN(a)));*/
